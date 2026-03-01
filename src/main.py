71436493a729b2ab4e70b83c33b77c52028e8cc2
aumezawa <letzt.apostel@gmail.com>
2026-03-01 19:49:04 +0900

    v1.1.0: Added middleware

--- src/main.py
+++ src/main.py
@@ -1,17 +1,49 @@
 """
 main.py
 
-version : 1.0.0
+version : 1.1.0
 author  : aumezawa
 """
 
-from fastapi import FastAPI
+from __future__ import annotations
+
+from typing import Any, override
+
+from fastapi import FastAPI, Request, Response
 from fastmcp import FastMCP
+from fastmcp.server.middleware import CallNext, Middleware, MiddlewareContext
+from loguru import logger
+from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
 
 from src.libs.logger import setup_logger
 from src.tools.currency_rate import get_exchange_rate
 
 setup_logger(["uvicorn", "uvicorn.access"])
+
+
+class CustomMcpMiddleware(Middleware):
+    """Custom MCP Middleware Class."""
+
+    @override
+    async def on_message(
+        self,
+        context: MiddlewareContext[Any],
+        call_next: CallNext[Any, Any],
+    ) -> Any:
+        """Handle before and after an MCP request."""
+        logger.debug(f"context: {context}")
+        return await call_next(context)
+
+
+class CustomHttpMiddleware(BaseHTTPMiddleware):
+    """Custom HTTP Middleware Class."""
+
+    @override
+    async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Response:
+        """Handle before and after a HTTP request."""
+        body = await request.body()
+        logger.debug(f"method: {request.method}, headers: {request.headers}, body: {body.decode()}")
+        return await call_next(request)
 
 
 mcp = FastMCP(
@@ -24,12 +56,15 @@ mcp = FastMCP(
     strict_input_validation=True,
 )
 
+mcp.add_middleware(CustomMcpMiddleware())
+
 mcp_app = mcp.http_app(
     path="/",
     transport="streamable-http",
 )
 
 app = FastAPI(lifespan=mcp_app.lifespan)
+app.add_middleware(CustomHttpMiddleware)
 app.mount("/mcp", mcp_app)
 
 
